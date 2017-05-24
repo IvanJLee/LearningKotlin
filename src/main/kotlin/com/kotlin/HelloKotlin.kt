372661d95c0800cf98fb9bfcaa474e7fdac2b669
lijun <lijun@weibangong.com>
2017-05-24 16:53:16 +0800

    hello kotlin

---
+++ src/main/kotlin/com/kotlin/HelloKotlin.kt
@@ -0,0 +1,14 @@
+package com.kotlin
+
+/**
+ * Hello kotlin
+ *
+ * @author  Ivan on 2017/5/24 16:04.
+ * @version v0.1
+ * @since   v1.0
+ *
+ */
+
+fun main(args: Array<String>) {
+    println("hello kotlin")
+}
