829082ba087f12fe4b60fe76e313bf650de2a988
Ivan J. Lee <lijun@weibangong.com>
2017-05-31 11:45:21 +0800

    conditionnal expressions

--- src/main/kotlin/com/kotlin/BasicSyntax.kt
+++ src/main/kotlin/com/kotlin/BasicSyntax.kt
@@ -36,6 +36,7 @@ fun log2(msg : String) {
 }
 
 fun main(args: Array<String>) {
+    println("function definition: ")
     //functions
     println("sum:  2 + 4 = " + sum(2,4))
     println("sum2: 3 + 4 = " + sum2(3,4))
@@ -43,6 +44,7 @@ fun main(args: Array<String>) {
     log2("function with no returning type")
     funDefineBelowMain()
 
+    println("\nvariables definition: ")
     //local variables, Assign-once(read-only) variable
     val a : Int = 3
     val b = 3
@@ -60,9 +62,12 @@ fun main(args: Array<String>) {
     println("x = " + x )
     println("y = " + y )
     println("z = " + z )
+    println()
 
     //string templates
     stringTemplates()
+    //conditional expressions
+    conditionalExpressions(3, 4)
 }
 
 /**
@@ -76,9 +81,24 @@ fun funDefineBelowMain(): Unit {
  * In a string, variables followed after '$' will be replaced with its value. In another case, expressions surrounded with '{}' can be placed after '$'.
  */
 fun stringTemplates() {
+    println("string templates: ")
     var a = 1
     val str = "a is $a"
     a = 2
     val s = "${str.replace("is", "was")} , but now is $a"
     println(s)
+    println()
+}
+
+/**
+ * There is nothing different from Java when using 'if' and 'else' expression in most cases. So I'm not intent on writing these kind of demos.
+ * The different usage is below.
+ *
+ * Notice: ?: does not exists in Kotlin
+ */
+fun conditionalExpressions(a : Int, b: Int) {
+    println("conditional expressions: ")
+    val c = if (a > b) a else b
+    print("c = $c")
+    println()
 }
