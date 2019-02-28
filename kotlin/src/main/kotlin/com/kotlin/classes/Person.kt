b5ba6e6849425613a218aa4ba19254b35976ac76
Ivan J. Lee <lijundut@foxmail.com>
2019-02-28 16:40:57 +0800

    classes

--- kotlin/src/main/kotlin/com/kotlin/classes/Person.kt
+++ kotlin/src/main/kotlin/com/kotlin/classes/Person.kt
@@ -89,13 +89,15 @@ class Biosphere(name: String) {
  * If the primary constructor does not have any annotations or visibility modifiers, the constructor
  * keyword can be omitted:
  */
-class Animal(species: String) {
+open class Animal(species: String) {
 
     /**
      * Parameters of the primary constructor can be used in property initializers declared in the
      * class body:
      */
     val s = species[0]
+
+    open val x: Int = 1
 
     /**
      * Parameters of the primary constructor can also be used in the initializer blocks.
@@ -116,14 +118,92 @@ class Plant(val species: String, var age: Int) {} // the properties declared in 
  * If the constructor has annotations or visibility modifiers, the constructor keyword is required,
  * and the modifiers go before it:
  */
-class Microorganism public @Inject constructor(species: String) {}
+class Microorganism public @Inject constructor(species: String) {
 
-/**
- * If you do not want your class to have a public constructor, you need to declare an empty primary
- * constructor with non-default visibility
- */
-class DontCreateMe private constructor() {}
+    /**
+     * If you do not want your class to have a public constructor, you need to declare an empty primary
+     * constructor with non-default visibility
+     */
+    private constructor() :this("") {}
+}
+
+class Dog: Animal(species = "Dog") {
+    /**
+     * override properties
+     * the property must have a compatible type.
+     * val property can be overrided to var
+     * open property can be overided to final
+     */
+     final override var x: Int = 2
+}
+
+open class Parent() {
+
+    init {
+        println("Initializing Parent")
+    }
+
+    open val name: String = "Parent".also { println("Initializing name in Parent") }
+    open val x: Int get() = 30
+
+    open fun f() {
+        println("f() in parent")
+    }
+}
+
+class Child : Parent() {
+
+    /**
+     * child init happens after parent, so as property
+     */
+    init {
+        println("Initializing Child")
+    }
+
+    override val name: String = "Child".also { println("Initializing name in Child") }
+    override val x: Int = super.x + 1
+
+    /*
+     * call function in parent
+     */
+    override fun f() {
+        super.f()
+        println("f() in Child")
+    }
+
+    inner class Son public constructor() {
+        fun g() {
+            super@Child.f() // Call Parent's implementation
+            println(super@Child.x) // Uses Parent's implementation of x's getter
+        }
+    }
+
+    fun test() {
+        Son().g()
+    }
+}
+
+interface Speak {
+    fun f() { println("f() in Speak")} // interface functions can have default implementation ,they are 'open' by default
+}
+
+class Kid: Parent(), Speak {
+    override fun f() {
+        super<Parent>.f() // call Parent's f()
+        super<Speak>.f() // call Speak's f()
+    }
+}
+
+abstract class Children : Parent() {
+
+    abstract override fun f() // override can make a nonabstract function abstract. Abstract funtion has no body
+}
 
 fun main() {
+    //create instance of a class
     val bio = Biosphere("Alphabeta")
+
+    val child = Child()
+    child.test()
+
 }
